4cc85b2c99522402932bec915d4819c0d9bc363a
iabdinur <iabdinur1@gmail.com>
2023-09-19 23:26:13 +0100

    Post create updated delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import (
     PostListView,
     PostDetailView,
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    re_path(r'^user/(?P<username>\w{0,50})/$', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
